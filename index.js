34a6f660d3f0219aefedf77d0a67d5e50b1b8aaa
jsonjj <44034446+jsonjj@users.noreply.github.com>
2024-06-12 18:21:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
+const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const functions = require('./functions');
 
-const maxDays = 180;
+const app = express();
+const port = 3000;
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/calculate', (req, res) => {
+  const { gradesInput, attendanceInput } = req.body;
+
+  const gradeContents = fs.readFileSync('./grades.txt').toString();
+  const attendanceContents = fs.readFileSync('./attendance.txt').toString();
+
+  const grades = functions.stringToGrades(gradeContents);
+  const attendance = functions.stringToAttendance(attendanceContents);
+
+  let gradeStats = functions.calculateStats(grades.flat());
+  let attendanceStats = functions.calculateStats(attendance, false);
+
+  let attendanceChange = functions.calculateDrasticChange(attendanceStats, Number(attendanceInput));
+  let gradeChange = 0;
+  let count = 0;
+  for (let gradeInput of gradesInput.split(',')) {
+    count++;
+    gradeChange += functions.calculateDrasticChange(gradeStats, Number(gradeInput), true);
+  }
+  gradeChange /= count;
+
+  res.json({ gradeChange, attendanceChange });
 });
 
-readline.question('Enter grades (separated by commas): ', (gradesInput) => {
-  readline.question('Enter number of tardies: ', (attendanceInput) => {
-    const gradeContents = fs.readFileSync('./grades.txt').toString();
-    const attendanceContents = fs.readFileSync('./attendance.txt').toString();
-    
-    const grades = functions.stringToGrades(gradeContents);
-    const attendance = functions.stringToAttendance(attendanceContents);
-    
-    let gradeStats = functions.calculateStats(grades.flat());
-    let attendanceStats = functions.calculateStats(attendance, false);
-    
-    let attendanceChange = functions.calculateDrasticChange(attendanceStats, Number(attendanceInput));
-    let gradeChange = 0;
-    let count = 0;
-    for (let gradeInput of gradesInput.split(',')) {
-      count++;
-      gradeChange += functions.calculateDrasticChange(gradeStats, Number(gradeInput), true);
-    }
-    gradeChange /= count;
-    console.log(gradeChange);
-    console.log(attendanceChange);
-    readline.close();
-  });
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
